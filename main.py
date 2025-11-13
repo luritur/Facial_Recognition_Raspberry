8f22d2bea29365b7ce7177c5b72ff61176b44432
luritur <lur.iturbide@opendeusto.es>
2025-11-13 10:50:26 +0100

    estructura grabar video

---
+++ main.py
@@ -0,0 +1,18 @@
+#import cv2
+#
+#cap = cv2.VideoCapture(0)  # Abre la cámara
+
+#while True:
+#    ret, frame = cap.read()
+#    if not ret:
+#        print("No se pudo leer el frame.")
+#        break
+
+#   cv2.imshow("Cámara", frame)  # Muestra el frame en una ventana llamada "Cámara" (camara/ventana)
+
+    # Espera 1 ms y revisa si se presionó la tecla ESC (código 27)
+#    if cv2.waitKey(x) & 0xFF == 27:    x=tiempo de cada frame (1000/60)
+#        break
+
+#cap.release()
+#cv2.destroyAllWindows()
