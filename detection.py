9ec25d440debd4222e5201a9b2c9ba943f676fe9
Davidrs05 <david.r@opendeusto.es>
2025-11-27 11:40:11 +0100

    links

--- detection.py
+++ detection.py
@@ -1,23 +1,18 @@
 import mediapipe as mp
 import queue_class as queue
 import cv2
-import threading
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
 frames = queue.frames
 
 
 
-
-
 def detection_run():
     print("detectando en detection_run....")
-    with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection: #with para liberar recursos automaticamente
+    with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection: #'with' para liberar recursos automaticamente
         while(True):
             frame = frames.get()
-            #aqui hacer la deteccion con mediapipe
-            #los detectados añadirlos a la cola de 
-            
+
             # To improve performance, optionally mark the image as not writeable to
             # pass by reference.
             frame.flags.writeable = False
@@ -28,13 +23,14 @@ def detection_run():
             frame.flags.writeable = True
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
             if results.detections: #en results se guarda una lista de caras detectadas
-                print("CARA DETECTADA !!!!")
+                print("CARA DETECTADA !!!!!!!")
                 for detection in results.detections:
+                    queue.detected.put(frame) #pasamos frame a la cola para reconocer 
                     mp_drawing.draw_detection(frame, detection) #modifica el frame y le pone un rectangulo 
                     queue.show_queue.put(frame)
-                    # Flip the image horizontally for a selfie-view display.
-                    cv2.imshow('MediaPipe Face Detection', cv2.flip(frame, 1))
+                    
             else: 
+                print("cara no detectada------")
                 queue.show_queue.put(frame)
 
 
--- recognition.py
+++ recognition.py
@@ -1,3 +1,3 @@
-
+#con LBPH: https://www.geeksforgeeks.org/computer-vision/face-recognition-with-local-binary-patterns-lbps-and-opencv/?utm_source=chatgpt.com
 def recognition_run():
     print("reconociendo")
