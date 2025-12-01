8bbb96eb5f4a4462a07e5b7e3af02ef97498407b
Davidrs05 <david.r@opendeusto.es>
2025-12-01 18:37:11 +0100

    empezado a implementar el reconocimiento facial con lbph. A TENER EN CUENTA: -1: como hacer para acabar con los while True de los hilos detection_run y recognition_run? -2: OJO ver realmente cuales son las dimensions (detection.py)

--- recognition.py
+++ recognition.py
@@ -1,3 +1,27 @@
 #con LBPH: https://www.geeksforgeeks.org/computer-vision/face-recognition-with-local-binary-patterns-lbps-and-opencv/?utm_source=chatgpt.com
+
+import cv2
+import os
+import numpy as np
+import queue_class as queue
+
+frames = queue.detected
+
+
+
 def recognition_run():
     print("reconociendo")
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    while(True):
+        #coger el frame de la cola frames 
+        #reconocer el frame 
+
+
+
+
+
+
+# RECOGNIZE faces: 
+    #1. detectar cara con detection.py del frame que llega 
+    #2. pasarlo a grayScale 
+    #3. detectar usando el modelo entrenado .xml
